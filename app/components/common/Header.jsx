45142347878376e34973f60f21c1d7f92700abf8
aloshvp <aloshvp12@gmail.com>
2025-12-16 06:16:25 +0530

    whatsapp btn

--- app/components/common/Header.jsx
+++ app/components/common/Header.jsx
@@ -19,51 +19,66 @@ const Header = () => {
     }, []);
 
     return (
-        <header className={`headerWrap ${isScrolled ? "scrolled" : ""}`}>
-            <div className="container">
-                <div className="headerSec">
-                    <div className="hamburger">
-                        <span className="mrgnLft"></span>
-                        <span></span>
-                        <span className="mrgnLft"></span>
-                    </div>
+        <>
+            <header className={`headerWrap ${isScrolled ? "scrolled" : ""}`}>
+                <div className="container">
+                    <div className="headerSec">
+                        <div className="hamburger">
+                            <span className="mrgnLft"></span>
+                            <span></span>
+                            <span className="mrgnLft"></span>
+                        </div>
 
-                    <div className="headerNavLinks">
-                        <ul className="navLinksList">
-                            <li><Link href="/" className="homeLink">Home</Link></li>
-                            <li><Link href="">About Us</Link></li>
-                            <li><Link href="">Products</Link></li>
+                        <div className="headerNavLinks">
+                            <ul className="navLinksList">
+                                <li><Link href="/" className="homeLink">Home</Link></li>
+                                <li><Link href="">About Us</Link></li>
+                                <li><Link href="">Products</Link></li>
 
-                            <li className="headerLogoSec">
-                                <Link href="/" scroll={false} className="headerLogo">
-                                    <Image
-                                        src="/images/common/QahwaVentures-logo.png"
-                                        width={163}
-                                        height={164}
-                                        alt="Qahwa Ventures"
-                                    />
-                                </Link>
-                            </li>
+                                <li className="headerLogoSec">
+                                    <Link href="/" scroll={false} className="headerLogo">
+                                        <Image
+                                            src="/images/common/QahwaVentures-logo.png"
+                                            width={163}
+                                            height={164}
+                                            alt="Qahwa Ventures"
+                                        />
+                                    </Link>
+                                </li>
 
-                            <li><Link href="">Specialities</Link></li>
-                            <li><Link href="">Career</Link></li>
-                            <li><Link href="">Contact Us</Link></li>
-                        </ul>
-                    </div>
+                                <li><Link href="">Specialities</Link></li>
+                                <li><Link href="">Career</Link></li>
+                                <li><Link href="">Contact Us</Link></li>
+                            </ul>
+                        </div>
 
-                    <div className="talkWithUsSec">
-                        <span>Talk with us</span>
-                        <Image
-                            src="/images/common/phone.png"
-                            width={24}
-                            height={24}
-                            alt="Qahwa Ventures"
-                            className="talkWithUsIcon"
-                        />
+                        <div className="talkWithUsSec">
+                            <span>Talk with us</span>
+                            <Image
+                                src="/images/common/phone.png"
+                                width={24}
+                                height={24}
+                                alt="Qahwa Ventures"
+                                className="talkWithUsIcon"
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </header>
+            </header>
+
+            {isScrolled && (
+                <div className="whatsappBtnWrap">
+                    <Link href="" target="_blank" rel="noopener noreferrer" className="whatsappBtn">
+                        <Image
+                            src="/images/common/whatsapp.png"
+                            width={38}
+                            height={38}
+                            alt="WhatsApp"
+                        />
+                    </Link>
+                </div>
+            )}
+        </>
     );
 };
 
